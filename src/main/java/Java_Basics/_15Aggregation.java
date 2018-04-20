970844506fed04798839889a29093f02770fa601
Karteek Ganji <karteek.ganji@thrymr.net>
2018-04-20 08:51:40 +0530

    Association

--- src/main/java/Java_Basics/_15Aggregation.java
+++ src/main/java/Java_Basics/_15Aggregation.java
@@ -92,7 +92,7 @@ public class _15Aggregation {
 			StudentClass obj = new StudentClass(123, "Chaitanya", ad);
 			System.out.println(obj.rollNum);
 			System.out.println(obj.studentName);
-			System.out.println(obj.studentAddr);
+			System.out.println(obj.studentAddr.streetNum);
 			System.out.println(obj.studentAddr.city);
 			System.out.println(obj.studentAddr.state);
 			System.out.println(obj.studentAddr.country);
---
+++ src/main/java/Java_Basics/_16Association.java
@@ -0,0 +1,44 @@
+package Java_Basics;
+
+public class _16Association {
+	
+	/*
+	 * Association establishes relationship between two separate classes through
+	 * their objects. The relationship can be one to one, One to many, many to
+	 * one and many to many.
+	 * 
+	 * Association Example
+	 */
+
+	static class CarClass{
+	   String carName;
+	   int carId;
+	   CarClass(String name, int id)
+	   {
+		this.carName = name;
+		this.carId = id;
+	   }
+	}
+	static class Driver extends CarClass{
+	   String driverName;
+	   Driver(String name, String cname, int cid){
+		super(cname, cid);
+		this.driverName=name;
+	   }
+	}
+	static class TransportCompany{
+	   public static void main(String args[])
+	   {
+		Driver obj = new Driver("Andy", "Ford", 9988);
+		System.out.println(obj.driverName+" is a driver of car Id: "+obj.carId);
+	   }
+	}
+	
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
