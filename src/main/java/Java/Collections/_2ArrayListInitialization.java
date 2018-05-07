ca4c1c7374e8bc97cbe8a3a9b218b1b395a87ec9
Karteek Ganji <karteek.ganji@thrymr.net>
2018-05-07 08:30:39 +0530

    Added ArrayLists loops and initializations

--- src/main/java/Java/Collections/_2ArrayListInitialization.java
+++ src/main/java/Java/Collections/_2ArrayListInitialization.java
@@ -2,6 +2,7 @@ package Java.Collections;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class _2ArrayListInitialization {
 
@@ -11,7 +12,52 @@ public class _2ArrayListInitialization {
 		public static void main(String args[]) {
 			ArrayList<String> obj = new ArrayList<String>(Arrays.asList("Pratap", "Peter", "Harsh"));
 			System.out.println("Elements are:" + obj);
+			
 		}
 	}
 	
+	// Method 2: Anonymous inner class method to initialize ArrayList
+
+	@SuppressWarnings("serial")
+	public static class InitializationExample2 {
+		public static void main(String args[]) {
+			ArrayList<String> cities = new ArrayList<String>() {
+				{
+					add("Delhi");
+					add("Agra");
+					add("Chennai");
+				}
+			};
+			System.out.println("Content of Array list cities:" + cities);
+		}
+	}
+	
+	// Method3: Normal way of ArrayList initialization
+
+	public static class InitializationExample3 {
+		public static void main(String args[]) {
+			ArrayList<String> books = new ArrayList<String>();
+			books.add("Java Book1");
+			books.add("Java Book2");
+			books.add("Java Book3");
+			System.out.println("Books stored in array list are: " + books);
+		}
+	}
+	
+	
+	// Method 4: Use Collections.ncopies
+	
+	/**
+	 * Collections.ncopies method can be used when we need to initialize the
+	 * ArrayList with the same value for all of its elements. Syntax: count is
+	 * number of elements and element is the item value
+	 */
+
+	public static class InitializationExample4 {
+		public static void main(String args[]) {
+			ArrayList<Integer> intlist = new ArrayList<Integer>(Collections.nCopies(10, 5));
+			System.out.println("ArrayList items: " + intlist);
+		}
+	}
+
 }
---
+++ src/main/java/Java/Collections/_3ArrayListLoops.java
@@ -0,0 +1,83 @@
+package Java.Collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Iterator;
+
+public class _3ArrayListLoops {
+
+	/*
+	 * There are four ways to loop ArrayList:
+	 * 
+	 * For Loop
+	 * Advanced for loop
+	 * While Loop 
+	 * Iterator
+	 */
+	
+	public static class LoopExample {
+		public static void main(String[] args) {
+
+			ArrayList<Integer> arrlist = new ArrayList<Integer>();
+			arrlist.add(7);
+			arrlist.add(14);
+			arrlist.add(21);
+			arrlist.add(28);
+
+			/* For Loop for iterating ArrayList */
+			System.out.println("For Loop");
+			for (int counter = 0; counter < arrlist.size(); counter++) {
+				System.out.println(arrlist.get(counter));
+			}
+
+			/* Advanced For Loop */
+			System.out.println("Advanced For Loop");
+			for (Integer num : arrlist) {
+				System.out.println(num);
+			}
+
+			/* While Loop for iterating ArrayList */
+			System.out.println("While Loop");
+			int count = 0;
+			while (arrlist.size() > count) {
+				System.out.println(arrlist.get(count));
+				count++;
+			}
+
+			/* Looping Array List using Iterator */
+			System.out.println("Iterator");
+			Iterator iter = arrlist.iterator();
+			while (iter.hasNext()) {
+				System.out.println(iter.next());
+				
+			}
+
+		}
+		
+		public static class EnumerationExample {
+
+			public static void main(String[] args) {
+				// create an ArrayList object
+				ArrayList<String> arrayList = new ArrayList<String>();
+
+				// Add elements to ArrayList
+				arrayList.add("C");
+				arrayList.add("C++");
+				arrayList.add("Java");
+				arrayList.add("DotNet");
+				arrayList.add("Perl");
+
+				// Get the Enumeration object
+				Enumeration<String> e = Collections.enumeration(arrayList);
+
+				// Enumerate through the ArrayList elements
+				System.out.println("ArrayList elements: ");
+				while (e.hasMoreElements())
+					System.out.println(e.nextElement());
+			}
+		}
+
+}
+
+}
