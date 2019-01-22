62fec7d2011718a878ef789de6b09f63fc233f3c
karteekganji <karteekg123@gmail.com>
2019-01-22 20:36:10 +0530

    Practices

--- src/main/java/Dummy/Practice.java
+++ src/main/java/Dummy/Practice.java
@@ -17,13 +17,74 @@ public class Practice {
 
 	public static void main(String[] args) {
 
-		Practice object = new Practice();
-		Practice object2 = new Practice();
-		object.value();
-		object.show();
-		System.out.println("------------");
-		object2.value();
-		object2.show();
+		/*
+		 * Practice object = new Practice(); 
+		 * Practice object2 = new Practice();
+		 * object.value(); object.show(); 
+		 * System.out.println("------------");
+		 * object2.value(); object2.show();
+		 */
+		// printingStarsDescending();
+//		printingStarsAscending();
+		printingStarsAscendingReverse();
+	}
+
+	/*
+	 ****
+	 ***
+	 **
+	 *
+	 */
+	public static void printingStarsDescending() {
+		int limit = 5;
+		for (int i = limit; i >= 1; i--) {
+			for (int j = i; j >= 1; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+
+	}
+
+	/*
+	 * 
+	 **
+	 ***
+	 ****
+	 */
+	public static void printingStarsAscending() {
+		int limit = 5;
+		for (int i = 1; i <= limit; i++) {
+			for (int j = i; j >= 1; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+
+	}
+
+	/*
+	 	 * 
+	 	**
+	   ***
+	  ****
+	 */
+	public static void printingStarsAscendingReverse() {
+		int value = 1;
+		int limit = 4;
+		for (int i = limit; i >= value; i--) {
+			System.out.print(".");
+			if (i == value) {
+				for (int j = value; j <= value ; j++) {
+					System.out.print("*");
+				}
+				value ++;
+				System.out.println();
+			}
+			
+		}
 	}
 
 }
